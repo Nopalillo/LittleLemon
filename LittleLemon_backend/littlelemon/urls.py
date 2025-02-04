14487f7e354c47b8158132b2b2763ab1b981d9bd
Nopalillo <pepechu159@gmail.com>
2025-02-04 13:48:09 -0600

    Adding authentication to the project

--- LittleLemon_backend/littlelemon/urls.py
+++ LittleLemon_backend/littlelemon/urls.py
@@ -18,12 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from restaurant.views import BookingViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'tables', BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token),
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),  
 ]
